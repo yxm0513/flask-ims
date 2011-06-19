8ad709d44943a0ffa20acbf02e5cb0b572540afb
XinMing <simon.yang.sh@gmail.com>
2011-06-19 15:02:26 +0800

    baisc

--- ims/main.py
+++
@@ -1,4 +0,0 @@
-from google.appengine.ext.webapp.util import run_wsgi_app
-import ims
-
-run_wsgi_app(ims.app)
--- ims/setting.py
+++
@@ -1,6 +0,0 @@
-# configuration
-PROJECT = r'C:\Users\Simon\workspace\flask-ims\\'
-UPLOAD_FOLDER = PROJECT + r'uploads\\'
-DATABASE = PROJECT + r'\db\ims.db'
-DEBUG = True
-SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
