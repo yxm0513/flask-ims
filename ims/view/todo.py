d7e05886785f34dbf72a865e53f213a5f89b5653
xinming <xinming@xinming-desktop>
2011-06-22 12:36:04 +0800

    up

---
+++ ims/view/todo.py
@@ -0,0 +1,41 @@
+from flask import Module, request, flash, url_for, redirect, \
+     render_template, abort
+from flaskext.sqlalchemy import SQLAlchemy
+from ims.models import db, Todo
+
+
+mod = Module(__name__)
+
+
+@mod.route('/todo')
+def show_all():
+    return render_template('todo/show_all.html',
+        todos=Todo.query.order_by(Todo.pub_date.desc()).all()
+    )
+
+
+@mod.route('/todo/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['title']:
+            flash('Title is required', 'error')
+        elif not request.form['text']:
+            flash('Text is required', 'error')
+        else:
+            todo = Todo(request.form['title'], request.form['text'])
+            db.session.add(todo)
+            db.session.commit()
+            flash(u'Todo item was successfully created')
+            return redirect(url_for('show_all'))
+    return render_template('todo/new.html')
+
+
+@mod.route('/todo/action', methods=['POST'])
+def update_done():
+    for todo in Todo.query.all():
+        todo.done = ('done.%d' % todo.id) in request.form
+        if ('del.%d' % todo.id) in request.form:
+            db.session.delete(todo)
+    flash('Updated status')
+    db.session.commit()
+    return redirect(url_for('show_all'))
